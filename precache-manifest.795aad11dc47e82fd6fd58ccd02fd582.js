2dcdacb1d66e7e20d526fcbfa2c309dd4413d37b
deepmandlik <mandlikdeep20@gmail.com>
2020-07-21 14:47:02 +0530

    Updates

---
+++ precache-manifest.795aad11dc47e82fd6fd58ccd02fd582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac4593681abf396d18c21f8ef6a5039",
+    "url": "/Simple-Calculator/index.html"
+  },
+  {
+    "revision": "0ebd0a025d334582dd6a",
+    "url": "/Simple-Calculator/static/css/main.ffc5e760.chunk.css"
+  },
+  {
+    "revision": "5a653fcb7c9c94406304",
+    "url": "/Simple-Calculator/static/js/2.c06a8002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Calculator/static/js/2.c06a8002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebd0a025d334582dd6a",
+    "url": "/Simple-Calculator/static/js/main.78ba2b00.chunk.js"
+  },
+  {
+    "revision": "74068f36181c1151e082",
+    "url": "/Simple-Calculator/static/js/runtime-main.6a1cde54.js"
+  }
+]);
